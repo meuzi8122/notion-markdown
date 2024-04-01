eeb2f7cce80802c8a959afccb4099ffe715106fe
meuzi8122 <kagiakete1234@gmail.com>
2024-04-01 19:00:06 +0900

    * fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import { NotionPageClient } from "./notion-page-client";
 
-const DIR_PATH = "./markdown";
+const DIR_PATH = "./memos";
 
 for (let subDirName of fs.readdirSync(DIR_PATH)) {
     for (let fileName of fs.readdirSync(`${DIR_PATH}/${subDirName}`)) {
